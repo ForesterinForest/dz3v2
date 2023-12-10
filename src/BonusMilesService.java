5324b3c3b3d03a8536c4d385256a9cbb8c659419
Nikolay <masknik1666@yandex.ru>
2023-12-10 17:46:10 +0300

    Initial commit

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,9 +1,7 @@
 public class BonusMilesService {
-    int bonus;
     public int calculate(int price) {
-       bonus = price / 20;
-        return  bonus;
-
+        int miles = price / 20;
+        return miles;
 
 
     }
